e54340ff1e1057759c9d72f80643ae026f11afea
Ramachandren Srinivasa Rao <ramachandren@gmail.com>
2014-12-18 18:16:32 -0500

    fix .spacemacs link

--- setup-links.sh
+++ setup-links.sh
@@ -6,5 +6,5 @@ mkdir ~/Documents/config-ram
 git clone --recursive http://github.com/syl20bnr/spacemacs ~/Documents/config-ram/spacemacs
 git clone https://github.com/tux4life/dot-spacemacs ~/Documents/config-ram/dot-spacemacs
 ln -s ~/Documents/config-ram/spacemacs ~/.emacs.d
-ln -s ~/Documents/config-ram/dot-spacemacs ~/.spacemacs
+ln -s ~/Documents/config-ram/dot-spacemacs/dot-spacemacs ~/.spacemacs
 ln -s ~/Documents/dotprofile/bash_profile ~/.bash_profile
