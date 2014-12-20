8809e6af66a4f7c0a82f68b4cb326983427b1d44
Ramachandren Srinivasa Rao <ramachandren@gmail.com>
2014-12-19 19:29:10 -0500

    Added iterm colors

--- setup-links.sh
+++ setup-links.sh
@@ -6,6 +6,7 @@ rm -rf ~/Documents/config-ram
 mkdir ~/Documents/config-ram
 git clone --recursive http://github.com/syl20bnr/spacemacs ~/Documents/config-ram/spacemacs
 git clone https://github.com/tux4life/dot-spacemacs ~/Documents/config-ram/dot-spacemacs
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git ~/Documents/config-ram/iterm-colors
 ln -s ~/Documents/config-ram/spacemacs ~/.emacs.d
 ln -s ~/Documents/config-ram/dot-spacemacs/dot-spacemacs ~/.spacemacs
 ln -s ~/Documents/dotprofile/bash_profile ~/.bash_profile
