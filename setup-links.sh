70008a9a71fa6849e0548ab0ae3b6322651607bf
Ramachandren Srinivasa Rao <ramachandren@gmail.com>
2014-12-20 13:30:23 -0500

    add vim to brewfile, move files to config-ram

--- setup-links.sh
+++ setup-links.sh
@@ -2,12 +2,11 @@ rm ~/.bash_profile
 rm ~/.emacs.d
 rm ~/.spacemacs
 rm ~/.zshrc
-rm -rf ~/Documents/config-ram
-mkdir ~/Documents/config-ram
-git clone --recursive http://github.com/syl20bnr/spacemacs ~/Documents/config-ram/spacemacs
-git clone https://github.com/tux4life/dot-spacemacs ~/Documents/config-ram/dot-spacemacs
-git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git ~/Documents/config-ram/iterm-colors
-ln -s ~/Documents/config-ram/spacemacs ~/.emacs.d
-ln -s ~/Documents/config-ram/dot-spacemacs/dot-spacemacs ~/.spacemacs
-ln -s ~/Documents/dotprofile/bash_profile ~/.bash_profile
-ln -s ~/Documents/dotprofile/zshrc ~/.zshrc
+mkdir ~/config-ram
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone --recursive http://github.com/syl20bnr/spacemacs ~/config-ram/spacemacs
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git ~/config-ram/iterm-colors
+ln -s ~/config-ram/spacemacs ~/.emacs.d
+ln -s ~/config-ram/dotprofile/dot-spacemacs ~/.spacemacs
+ln -s ~/config-ram/dotprofile/bash_profile ~/.bash_profile
+ln -s ~/config-ram/dotprofile/zshrc ~/.zshrc
