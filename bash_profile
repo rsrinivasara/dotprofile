d4ce1dcd3521a75a4767cdb550d25aba3fee3712
Ramachandren Srinivasa Rao <ramachandren@gmail.com>
2014-12-22 18:42:47 -0500

    adding tmux.conf

--- bash_profile
+++ bash_profile
@@ -1,3 +1,3 @@
 alias vim=/usr/local/bin/vim
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/opt/Caskroom"
-export PATH=${PATH}:~/Documents/config-ram/bin
+export PATH=${PATH}:~/config-ram/dotprofile/bin
--- setup-links.sh
+++ setup-links.sh
@@ -2,6 +2,7 @@ rm ~/.bash_profile
 rm ~/.emacs.d
 rm ~/.spacemacs
 rm ~/.zshrc
+rm ~/.tmuxrc.conf
 mkdir ~/config-ram
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone --recursive http://github.com/syl20bnr/spacemacs ~/config-ram/spacemacs
@@ -10,3 +11,4 @@ ln -s ~/config-ram/spacemacs ~/.emacs.d
 ln -s ~/config-ram/dotprofile/dot-spacemacs ~/.spacemacs
 ln -s ~/config-ram/dotprofile/bash_profile ~/.bash_profile
 ln -s ~/config-ram/dotprofile/zshrc ~/.zshrc
+ln -s ~/config-ram/dotprofile/tmux.conf ~/.tmux.conf
